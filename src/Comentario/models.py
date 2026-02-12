445def5dd81849dcffe6af4aa8290aa1e34c438b
Scorpion647 <jhoyflow15@gmail.com>
2026-02-12 18:15:38 -0500

    Se agregaron las aplicaciones creadas a settings.py, se aplicaron las migraciones y se crearon formularios con los datos a ingresar para cada tabla

--- src/Comentario/models.py
+++ src/Comentario/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from Ticket.models import Ticket
 
-# Create your models here.
+class Comentario(models.Model):
+
+    #se agrega en el modelo de la tabla de base de datos un nuevo campo que llame ticket para simplemente relacionar cada comentario a su ticket que corresponde
+    ticket = models.ForeignKey(Ticket,on_delete=models.CASCADE, related_name='Comentario')
+    Titulo = models.CharField(max_length=150, blank=False)
+    Descripcion = models.TextField()
+    fecha_registro = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comentario del ticket {self.ticket.id} con ID {self.id} - {self.fecha_registro}"
+
--- src/Ticket/models.py
+++ src/Ticket/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+class Ticket(models.Model):
+    SELEC_CATEGORIA = [
+        ('soporte', 'Soporte'),
+        ('Ventas', 'Ventas'),
+        ('facturacion', 'Facturacion'),
+        ('otros', 'Otros'),
+    ]
+    SELEC_PRIORIDAD = [
+        ('baja', 'Baja'),
+        ('media', 'Media'),
+        ('alta', 'Alta'),
+    ]
+    SELEC_ESTADO = [
+        ('abierto', 'Abierto'),
+        ('en_proceso', 'En proceso'),
+        ('resuelto', 'Resuelto'),
+    ]
+
+    titulo = models.CharField(max_length=150)
+    descripcion = models.TextField()
+    categoria = models.CharField(choices=SELEC_CATEGORIA)
+    prioridad = models.CharField(choices=SELEC_PRIORIDAD)
+    estado = models.CharField(choices=SELEC_ESTADO, default='abierto')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.id} - {self.titulo}"
+
+
+
